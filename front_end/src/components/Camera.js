a69b573a66e86f23bbec719d87d4cf5cbabd96e6
Elli Shim <ellishim@gmail.com>
2018-05-14 14:19:14 -0400

    webcam changes

--- front_end/src/components/Camera.js
+++ front_end/src/components/Camera.js
@@ -1,10 +1,26 @@
 import React, {Component} from 'react';
+import Webcam from 'react-webcam';
+import {Image} from 'semantic-ui-react'
 
 class Camera extends Component {
 
   render(){
     return (
-      <div>Camera</div>
+      <div>
+        <h2><center>Try it on!</center></h2>
+        <div className='webcam'>
+          <Webcam
+            id="content"
+            className="camera"
+            audio={false}
+            height={200}
+            ref={this.setRef}
+            screenshotFormat="image/jpeg"
+            width={350}
+          />
+          <Image className='glasses' size='small' src={require('../pictures/glasses.png')} />
+        </div>
+      </div>
     )
   }
 }
